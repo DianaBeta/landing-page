900071bfd7cb9fd4f9ee117d4276fe1820f20c02
dianabeta <dianabetancourt7@gmail.com>
2020-09-28 18:13:31 +0200

    style: some comments deleted and some added

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@
 const navbar = document.querySelector('#navbar__list');
 const sections = document.querySelectorAll('section');
 const section = document.querySelectorAll('.landing__container');
-console.log(section);
+const navbarlinks = document.querySelectorAll('navbar-links');
 
 /**
  * End Global Variables
@@ -39,64 +39,34 @@ document.body.onload = build_navbar()
 function build_navbar(){
     sections.forEach(build_navbar_entry);
 };
-
+//builds navbar by creating <li> elements for the empty navbar ul and adds the link according to sections. 
 function build_navbar_entry(section) {
     const title= section.dataset.nav;
     const listItem = document.createElement("li");
     const anchor =  document.createElement("A");
-    anchor.setAttribute('href','#' + section.id );
+    const linkName = anchor.setAttribute('href','#' + section.id );
+    //anchor.setAttribute('class',id);
+    anchor.classList.add("navbar-links"+ "-" + section.id);
     anchor.appendChild(document.createTextNode(title));
     listItem.appendChild(anchor);
     navbar.appendChild(listItem);
+    
 }
 
-
-// Add class 'active' to section when near top of viewport
-//make an event listener on scroll element that uses class list in the function body
-//GOAL add event listeners to each section= the event is scroll
-//function= changes the css by changing the active class
-// we need to focus in each active section change the old active section to inactive and change it to the current class in focus
-//class= active class;
-//remove the property active class from the no longer active section
-//1.add an event listener to section 2 that makes the background orange, at first section one can stay orange too but then the goal
-// is orange ( not hardcoding)
-//2 is everything works then we need to loop over the sections to highlight every active one
-// Scroll to anchor ID using scrollTO event
-
-/*var section2 = document.querySelectorAll('section')[1];
-section2.addEventListener("scroll", function(e) {
-     console.log("scroll triggered");
-});*/
+//highlights the active section by adding the class called "your-active-class"
 function makeActive(){ 
-    //const section= document.querySelector('#section1');
     for(const section of sections){
-    const listofClasses= section.classList;
-    console.log(listofClasses);
+     //getBoundingClientRect() function, returns the position of the current section in the viewport.   
     const box= section.getBoundingClientRect();
     if(box.top <=110 && box.bottom >= 110){
-    //section.style.backgroundColor = 'orange';
-    section.classList.add("your-active-class");
+    section.classList.add("your-active-class");   
         } else {
     section.classList.remove("your-active-class");
+    
         }
     }
 }
 
-    
-    /*function makeActive(){
-    for(const section of sections){
-    const box= section.getBoundingClientRect();
-        if(box.top <=150 && box.bottom >= 150) {
-        //Apply active state on the current section and correponding Nav link.
-            sections.classList.add("your-active-class");
-            console.log("the active class is"+ activeClass);
-            } else { 
-            sections.classList.remove("your-active-class");
-        //Remove active state from other section and corresponding Nav link.
-            }
-        }
-    }
-    */
 
 /**
  * End Main Functions
